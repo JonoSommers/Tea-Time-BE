fcb48e98fbb6eaf7ce51af39e69d23c32f8f38e9
Jono Sommers <jonosommers95@gmail.com>
2025-03-11 15:13:28 -0500

    Updaed subscription serializer to conditionaly render customers attribute. SHOW subscription endpoint working in postman and displays customers who are or were subscrbied.

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::SubscriptionsController < ApplicationController
     def index
-        render json: SubscriptionSerializer.new(Subscription.all), status: :ok
+        render json: SubscriptionSerializer.new(Subscription.all, { params: { include_customers: false } }), status: :ok
+    end
+
+    def show
+        subscription = Subscription.find(params[:id])
+        render json: SubscriptionSerializer.new(subscription, { params: { include_customers: true } }), status: :ok
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
         namespace :v1 do
             resources :customers, only: :index
             resources :teas, only: :index
-            resources :subscriptions, only: :index
+            resources :subscriptions, only: [:index, :show]
         end
     end
 end
