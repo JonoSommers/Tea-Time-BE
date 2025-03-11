f450ea3e8dc2c828415d728238affcf3aaf19bf6
Jono Sommers <jonosommers95@gmail.com>
2025-03-11 13:19:56 -0500

    Added GET request for teas, postman is returning all the correct data, test is passing.

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,5 +1,4 @@
 class Api::V1::CustomersController < ApplicationController
-
     def index
         render json: CustomerSerializer.new(Customer.all), status: :ok
     end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :api do
         namespace :v1 do
             resources :customers, only: :index
+            resources :teas, only: :index
         end
     end
 end
