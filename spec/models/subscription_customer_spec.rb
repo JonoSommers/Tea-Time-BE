017bb479952245d1754fee21435a87d1d8d7a037
Jono Sommers <jonosommers95@gmail.com>
2025-03-11 20:50:45 -0500

    model method updat_status is fully tested

--- spec/models/subscription_customer_spec.rb
+++ spec/models/subscription_customer_spec.rb
@@ -11,4 +11,28 @@ RSpec.describe SubscriptionCustomer, type: :model do
         it { should belong_to(:subscription) }
         it { should belong_to(:customer) }
     end
+
+    before :each do
+        @subscription = FactoryBot.create(:subscription)
+        @customer = FactoryBot.create(:customer)
+    end
+
+    describe 'self.update_status' do
+        it 'toggles the status and updates users_subscribed' do
+            subscription_customer = FactoryBot.create(:subscription_customer, subscription: @subscription, customer: @customer, status: true)
+            SubscriptionCustomer.update_status(@customer.id, @subscription, subscription_customer)
+            expect(subscription_customer.status).to eq(false)
+            expect(@subscription.users_subscribed).to eq(0) 
+        end
+
+        it 'creates a new subscription_customer and updates users_subscribed' do
+            new_subscription_customer = SubscriptionCustomer.find_by(customer_id: @customer.id, subscription_id: @subscription.id)
+            expect(new_subscription_customer).to be(nil)
+            new_subscription_customer = SubscriptionCustomer.update_status(@customer.id, @subscription, nil)
+            expect(new_subscription_customer).to_not be(nil)
+            expect(new_subscription_customer.customer_id).to eq(@customer.id)
+            expect(new_subscription_customer.subscription_id).to eq(@subscription.id)
+            expect(new_subscription_customer.status).to eq(true)
+        end
+    end
 end
